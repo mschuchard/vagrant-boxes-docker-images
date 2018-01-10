3bddc78ee2febfff641a1ff19ecc5ec944f034a6
Matt Schuchard <grapheneowns@gmail.com>
2018-01-10 12:12:36 -0500

    improve vagrant support and fix pe-vagrant paths

--- vagrant/vagrant_utilities.rb
+++ vagrant/vagrant_utilities.rb
@@ -1,27 +1,35 @@
-# Vagrant version checking
-raise 'Vagrant requires Cygwin or bash if on Windows.' if RUBY_PLATFORM =~ /mswin|msys|mingw|bccwin|wince|em[xc]/
+# BEGIN SETUP
+# store bools for efficiency
+windows = Vagrant::Util::Platform.windows?
+cygwin = Vagrant::Util::Platform.cygwin?
 
+# BEGIN ALWAYS EXECUTED
+raise 'Vagrant requires Cygwin or WSL if on Windows.' if windows && (!cygwin || !Vagrant::Util::Platform.wsl?)
+
+# Vagrant version checking
 Vagrant.require_version '>= 1.7.4'
 
 case Vagrant::VERSION
-when '1.9.4'
-  raise '1.9.4 is incompatible with Windows.' if Vagrant::Util::Platform.windows?
+when '1.9.4', '1.9.7'
+  raise '1.9.4 and 1.9.7 are incompatible with Windows.' if windows
 when '1.9.1'
-  warn '1.9.1 has potential issues with Redhat networking.'
+  warn '1.9.1 has potential issues with Redhat networking.' if Vagrant::Util::Platform.linux?
 when '1.9.0'
   warn '1.9.0 will error on plugin checks. You will need to manually install plugins and remove the relevant code for checking them from the Vagrantfile.'
 when '1.8.7'
-  raise '1.8.7 is incompatible with MacOSX.' if RUBY_PLATFORM =~ /darwin/
+  raise '1.8.7 is incompatible with MacOSX.' if Vagrant::Util::Platform.darwin?
 when '1.8.6'
-  warn '1.8.6 has potential issues with Linux networking.'
+  warn '1.8.6 has potential issues with Linux networking.' if Vagrant::Util::Platform.linux?
 when '1.8.5'
   raise '1.8.5 is broken.'
 end
 
+# BEGIN HELPER METHODS
 # install specified plugins
 def plugins_install(plugins)
   installed = false
-  sudo = RUBY_PLATFORM =~ /cygwin/ ? '' : 'sudo'
+  # cygwin is the one supported platform with no sudo
+  sudo = cygwin ? '' : 'sudo'
 
   plugins.each do |plugin|
     next if Vagrant.has_plugin?(plugin)
