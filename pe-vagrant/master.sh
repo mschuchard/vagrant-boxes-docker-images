7faaae064fccad19bc47b8ba43a898fd194a27ac
Matt Schuchard <grapheneowns@gmail.com>
2018-08-09 15:49:19 -0400

    updating pe and packer (for jenkins) versions

--- pe-vagrant/master.sh
+++ pe-vagrant/master.sh
@@ -3,4 +3,4 @@
 mkdir -p /vagrant/.pe_build/{post-install,answers}
 cp /vagrant/master.pp /vagrant/.pe_build/post-install/master.pp
 cp /vagrant/answers.txt /vagrant/.pe_build/answers/master.txt
-tar -xzf /vagrant/puppet-enterprise-2016.4.9-el-7-x86_64.tar.gz -C /vagrant/.pe_build/
+tar -xzf /vagrant/puppet-enterprise-2018.1.3-el-7-x86_64.tar.gz -C /vagrant/.pe_build/
