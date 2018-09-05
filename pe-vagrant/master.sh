916884dd25e7d5c17ae42c682cca673e1b071bd2
Matt Schuchard <grapheneowns@gmail.com>
2018-09-05 14:20:17 -0400

    updates and fixes for 2018.1.4 and newer vagrant plugin

--- pe-vagrant/master.sh
+++ pe-vagrant/master.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # Workarounds for erroneous vagrant-pe_build behavior regarding rsync.
-mkdir -p /vagrant/.pe_build/{post-install,answers}
+mkdir -p /vagrant/.pe_build/post-install
 cp /vagrant/master.pp /vagrant/.pe_build/post-install/master.pp
-cp /vagrant/answers.txt /vagrant/.pe_build/answers/master.txt
-tar -xzf /vagrant/puppet-enterprise-2018.1.3-el-7-x86_64.tar.gz -C /vagrant/.pe_build/
+tar -xzf /vagrant/puppet-enterprise-2018.1.4-el-7-x86_64.tar.gz -C /vagrant/.pe_build/
