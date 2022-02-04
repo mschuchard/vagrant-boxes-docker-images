c1a9a96664d2977525c5dcdfc0b087c1fee7ed1b
Matt Schuchard <grapheneowns@gmail.com>
2022-02-04 16:08:09 -0500

    add simplified and corrected atom ci script

---
+++ atom-build-package.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
+
+echo "Downloading latest Atom release on the ${ATOM_CHANNEL} channel..."
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+  curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
+    -H 'Accept: application/octet-stream' \
+    -o "atom-amd64.deb"
+  /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
+  export DISPLAY=":99"
+  dpkg-deb -x atom-amd64.deb "${HOME}/atom"
+  if [ "${ATOM_CHANNEL}" = "stable" ]; then
+    export ATOM_SCRIPT_NAME="atom"
+    export APM_SCRIPT_NAME="apm"
+  else
+    export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
+    export APM_SCRIPT_NAME="apm-${ATOM_CHANNEL}"
+  fi
+  export ATOM_SCRIPT_PATH="${HOME}/atom/usr/bin/${ATOM_SCRIPT_NAME}"
+  export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/${APM_SCRIPT_NAME}"
+  export NPM_SCRIPT_PATH="${HOME}/atom/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
+  export PATH="${PATH}:${HOME}/atom/usr/bin"
+elif [ "${CIRCLECI}" = "true" ]; then
+  # Assume the build is on a Debian based image (Circle CI provided Linux images)
+  curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
+    -H 'Accept: application/octet-stream' \
+    -o "atom-amd64.deb"
+  sudo dpkg --install atom-amd64.deb || true
+  sudo apt-get update
+  sudo apt-get --fix-broken --assume-yes --quiet install
+  if [ "${ATOM_CHANNEL}" = "stable" ] || [ "${ATOM_CHANNEL}" = "dev" ]; then
+    export ATOM_SCRIPT_PATH="atom"
+    export APM_SCRIPT_PATH="apm"
+  else
+    export ATOM_SCRIPT_PATH="atom-${ATOM_CHANNEL}"
+    export APM_SCRIPT_PATH="apm-${ATOM_CHANNEL}"
+  fi
+  export NPM_SCRIPT_PATH="/usr/share/atom/resources/app/apm/node_modules/.bin/npm"
+else
+  echo "Unknown CI environment, exiting!"
+  exit 1
+fi
+
+echo "Using Atom version:"
+"${ATOM_SCRIPT_PATH}" -v
+echo "Using APM version:"
+"${APM_SCRIPT_PATH}" -v
+
+echo "Downloading package dependencies..."
+"${APM_SCRIPT_PATH}" install
+"${APM_SCRIPT_PATH}" clean
+
+# Override the PATH to put the Node bundled with APM first
+if [ "${CIRCLECI}" = "true" ]; then
+  # Since CircleCI/Linux is a fully installed environment, we use the system path to apm
+  export PATH="/usr/share/atom/resources/app/apm/bin:${PATH}"
+else
+  export PATH="${HOME}/atom/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/bin:${PATH}"
+fi
+
+if [ -n "${APM_TEST_PACKAGES}" ]; then
+  echo "Installing atom package dependencies..."
+  for pack in ${APM_TEST_PACKAGES}; do
+    "${APM_SCRIPT_PATH}" install "${pack}"
+  done
+fi
+
+if [ -d ./lib ]; then
+  echo "Linting package using eslint..."
+  ./node_modules/.bin/eslint lib
+fi
+if [ -d ./spec ]; then
+  echo "Linting package specs using eslint..."
+  ./node_modules/.bin/eslint spec
+fi
+
+echo "Running specs..."
+"${ATOM_SCRIPT_PATH}" --test spec
+
+exit
