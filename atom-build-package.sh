7d82e11f2678d22afb8a49207f1f2900dabb5cce
Matt Schuchard <grapheneowns@gmail.com>
2023-01-27 10:47:43 -0500

    update atom ci build script

--- atom-build-package.sh
+++ atom-build-package.sh
@@ -1,40 +1,28 @@
 #!/bin/sh
-ATOM_CHANNEL="${ATOM_CHANNEL:=stable}"
-
-echo "Downloading latest Atom release on the ${ATOM_CHANNEL} channel..."
+echo "Downloading latest Atom release on the beta channel..."
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-  curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
+  curl -s -L "https://atom.io/download/deb?channel=beta" \
     -H 'Accept: application/octet-stream' \
     -o "atom-amd64.deb"
   /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
   export DISPLAY=":99"
   dpkg-deb -x atom-amd64.deb "${HOME}/atom"
-  if [ "${ATOM_CHANNEL}" = "stable" ]; then
-    export ATOM_SCRIPT_NAME="atom"
-    export APM_SCRIPT_NAME="apm"
-  else
-    export ATOM_SCRIPT_NAME="atom-${ATOM_CHANNEL}"
-    export APM_SCRIPT_NAME="apm-${ATOM_CHANNEL}"
-  fi
+  export ATOM_SCRIPT_NAME="atom-beta"
+  export APM_SCRIPT_NAME="apm-beta"
   export ATOM_SCRIPT_PATH="${HOME}/atom/usr/bin/${ATOM_SCRIPT_NAME}"
   export APM_SCRIPT_PATH="${HOME}/atom/usr/bin/${APM_SCRIPT_NAME}"
   export NPM_SCRIPT_PATH="${HOME}/atom/usr/share/${ATOM_SCRIPT_NAME}/resources/app/apm/node_modules/.bin/npm"
   export PATH="${PATH}:${HOME}/atom/usr/bin"
 elif [ "${CIRCLECI}" = "true" ]; then
   # Assume the build is on a Debian based image (Circle CI provided Linux images)
-  curl -s -L "https://atom.io/download/deb?channel=${ATOM_CHANNEL}" \
+  curl -s -L "https://atom.io/download/deb?channel=beta" \
     -H 'Accept: application/octet-stream' \
     -o "atom-amd64.deb"
   sudo dpkg --install atom-amd64.deb || true
   sudo apt-get update
   sudo apt-get --fix-broken --assume-yes --quiet install
-  if [ "${ATOM_CHANNEL}" = "stable" ] || [ "${ATOM_CHANNEL}" = "dev" ]; then
-    export ATOM_SCRIPT_PATH="atom"
-    export APM_SCRIPT_PATH="apm"
-  else
-    export ATOM_SCRIPT_PATH="atom-${ATOM_CHANNEL}"
-    export APM_SCRIPT_PATH="apm-${ATOM_CHANNEL}"
-  fi
+  export ATOM_SCRIPT_PATH="atom-beta"
+  export APM_SCRIPT_PATH="apm-beta"
   export NPM_SCRIPT_PATH="/usr/share/atom/resources/app/apm/node_modules/.bin/npm"
 else
   echo "Unknown CI environment, exiting!"
