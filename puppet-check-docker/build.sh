67cfec7f2553814a2058a0de702095aa8dd0a441
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-08-05 12:25:42 -0400

    updating images

--- puppet-check-docker/build.sh
+++ puppet-check-docker/build.sh
@@ -1,5 +1,5 @@
 IMAGE=puppet-check
-docker buildx build --pull . -t "matthewschuchard/${IMAGE}" -t "matthewschuchard/${IMAGE}:2" -t "matthewschuchard/${IMAGE}:2.5" -t "matthewschuchard/${IMAGE}:2.5.0"
+docker buildx build --pull . -t "matthewschuchard/${IMAGE}" -t "matthewschuchard/${IMAGE}:2" -t "matthewschuchard/${IMAGE}:2.5" -t "matthewschuchard/${IMAGE}:2.5.2"
 docker buildx build . --build-arg VERSION=2.4.0 -t "matthewschuchard/${IMAGE}:2.4" -t "matthewschuchard/${IMAGE}:2.4.0"
 docker buildx build . --build-arg VERSION=2.3.1 -t "matthewschuchard/${IMAGE}:2.3" -t "matthewschuchard/${IMAGE}:2.3.1"
 docker buildx build . --build-arg VERSION=2.2.2 -t "matthewschuchard/${IMAGE}:2.2" -t "matthewschuchard/${IMAGE}:2.2.2"
